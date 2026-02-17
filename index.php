fde57b4cb74b87ecbf4c143dfa5ca2fd019c521c
hannu64 <hannu.tomi@gmail.com>
2026-02-17 19:07:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ echo "<!DOCTYPE html>
 <body>
 
 <h1>SekuChat BackEnd</h1>
-<p>Testing seku.chat website index2</p>
+<p>Testing seku.chat website - this is a PHP file.</p>
 
 <p>This is BackEnd, python and DB here</p>
 <p>This BackEnd is on Railway</p>
+
+<p>Link to <a href='hello.py'>hello.py</a></p>
 
 </body>
 </html>"; 
